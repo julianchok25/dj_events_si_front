be7d6fda8ea854dca6e1249377253009a0f6f560
Julian Villegas <julian.villegasplus@gmail.com>
2020-06-05 20:39:56 -0500

    [ADD]page/SigninSignUp: splitting custom component and test boostrap components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import { Button, Alert } from "react-bootstrap";
+import SignInSingUp from "./page/SigninSingUp";
 
 export default function App() {
   const [user, setUser] = useState(null);
   // When user has a content, we want to say that the user is logged in
   return (
     <div>
-      {user ? <h1>Hi, you're logged in</h1> : <h1>You are not logged in</h1>};
+      {!user ? (
+        <div>
+          <SignInSingUp />
+        </div>
+      ) : (
+        <h1>You are not logged in</h1>
+      )}
+      <Button variant="primary">Button</Button>
+      <Alert variant="danger">This is a alert—check it out!</Alert>
     </div>
   );
 }
