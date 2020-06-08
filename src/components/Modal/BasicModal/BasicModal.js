0132bc0181623776ea5310692964519cc10e6f26
Julian Villegas <julian.villegasplus@gmail.com>
2020-06-07 22:49:54 -0500

    [ADD]BasicModal: Adding the Modal component inside the Signup page and design the UI

---
+++ src/components/Modal/BasicModal/BasicModal.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+
+import "./BasicModal.scss";
+
+export default function BasicModal(props) {
+  // This is destructuring
+  const { show, setShow, children } = props;
+  return (
+    <Modal
+      className="basic-modal"
+      show={show}
+      onHide={() => setShow(false)}
+      centered
+      size="lg"
+    >
+      <Modal.Header>
+        <Modal.Title>
+          <h2>Modal</h2>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{children}</Modal.Body>
+    </Modal>
+  );
+}
---
+++ src/components/Modal/BasicModal/index.js
@@ -0,0 +1,2 @@
+// This is for call the path file withouth /BasicModal/BasicMonal, only 1 time
+export { default } from "./BasicModal";
