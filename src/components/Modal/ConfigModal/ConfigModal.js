4f69a207453afd5b94694345b01f70802b7ddbf9
Julian Villegas <julian.villegasplus@gmail.com>
2020-06-14 16:44:39 -0500

    [ADD]ConfigModal: Preparing files to manage the edit profile modal

---
+++ src/components/Modal/ConfigModal/ConfigModal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+
+import "./ConfigModal.scss";
+
+export default function ConfigModal() {
+  return (
+    <div>
+      <h2>ConfigModal...</h2>
+    </div>
+  );
+}
