ef2d6d199289792cb1062559285e9c939434615c
Julian Villegas <julian.villegasplus@gmail.com>
2020-06-19 10:37:01 -0500

    Updates

---
+++ precache-manifest.468071a6d77de2a97603e49817f8a819.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccd37ee4a61193a031818e3e12fe341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b5b4d1382cc10c02f18",
+    "url": "/static/css/2.0c06fc5f.chunk.css"
+  },
+  {
+    "revision": "93d4b603c2e6dfe498f4",
+    "url": "/static/css/main.c9b3cb79.chunk.css"
+  },
+  {
+    "revision": "9b5b4d1382cc10c02f18",
+    "url": "/static/js/2.d0242283.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/static/js/2.d0242283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d4b603c2e6dfe498f4",
+    "url": "/static/js/main.3df8bde9.chunk.js"
+  },
+  {
+    "revision": "cd8a580a310521c29c42",
+    "url": "/static/js/runtime-main.d84bf5d8.js"
+  },
+  {
+    "revision": "7394195b74f13d80e58a0c1266d08443",
+    "url": "/static/media/VinylRed.7394195b.png"
+  },
+  {
+    "revision": "84f17a7054827c4121f0af1680b696f1",
+    "url": "/static/media/VinylWhite.84f17a70.png"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/static/media/birth-date.9b45fe49.svg"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/static/media/camera.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/static/media/link.a108520e.svg"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/static/media/location.02f2070b.svg"
+  }
+]);
